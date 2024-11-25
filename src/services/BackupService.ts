d03a319f7731ac927ad1d6fe0fc01b5734a31890
volognamor <romangolov2001@gmail.com>
2024-11-25 20:39:49 +0300

    change 7z on xcopy, refactoring backup method

--- src/services/BackupService.ts
+++ src/services/BackupService.ts
@@ -9,21 +9,38 @@ export class BackupService {
     try {
       Logger.log("Backup started!");
 
-      const { stdout, stderr } = await execute(
-        `${process.env.V_BOX_MANAGE_PATH} controlvm ${process.env.VM_NAME} poweroff && ${process.env.ARCHIVER_PATH} ${process.env.BACKUP_PATH} ${process.env.VM_FOLDER_PATH} && ${process.env.V_BOX_MANAGE_PATH} startvm ${process.env.VM_NAME} --type headless`,
-      );
-
-      if (stderr) {
-        Logger.log(stderr);
-      }
-
-      if (stdout) {
-        Logger.log(stdout);
-      }
+      await this.shutdownVM();
+      await this.backupVm();
+      await this.startupVm();
 
       Logger.log("Backup ended!");
     } catch (error) {
       if (error instanceof Error) return Logger.log(error.message);
     }
+  }
+
+  private async runExecute(command: string) {
+    Logger.log(command);
+    const { stdout, stderr } = await execute(command);
+
+    if (stdout) {
+      Logger.log(stdout);
+    }
+
+    if (stderr) {
+      Logger.log(stderr);
+    }
+  }
+
+  private async shutdownVM(): Promise<void> {
+    await this.runExecute(`${process.env.V_BOX_MANAGE_PATH} controlvm ${process.env.VM_NAME} poweroff`);
+  }
+
+  private async backupVm(): Promise<void> {
+    await this.runExecute(`${process.env.ARCHIVER_PATH} ${process.env.VM_FOLDER_PATH} ${process.env.BACKUP_PATH} ${process.env.BACKUP_ARGS}`);
+  }
+
+  private async startupVm(): Promise<void> {
+    await this.runExecute(`${process.env.V_BOX_MANAGE_PATH} startvm ${process.env.VM_NAME} --type headless`);
   }
 }
