e2d1a2826927b121b3b21b06b670d4bb632c4993
volognamor <romangolov2001@gmail.com>
2024-11-25 22:15:13 +0300

    add startup app message

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { BackupService } from "./services/BackupService";
 import cron from "node-cron";
+import Logger from "./helpers/Logger";
 
 const backupService = new BackupService();
 
 const rule = process.env.CRON_RULE ?? "0 3 * * *";
+
+Logger.logWithChannel('System loaded successfuly!').then();
 
 cron.schedule(rule, () => {
   backupService.start();
